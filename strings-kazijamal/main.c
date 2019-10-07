d5de574c61de7cbb7357fbc75e25bbf69016b1ae
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-07 11:25:32 -0400

    updated strings-kazijamal

--- strings-kazijamal/main.c
+++ strings-kazijamal/main.c
@@ -1,75 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "header.h"
 
-int main()
-{
-    char s1[32] = "??W?";
-    char s2[32] = "hello";
-    char s3[32] = "goodbye";
-    char mys1[32] = "??W?";
-    char mys2[32] = "hello";
-    char mys3[32] = "goodbye";
+int main() {
 
-    printf("s1: [%s]\n", s1);
-    printf("s2: [%s]\n", s2);
-    printf("s3: [%s]\n", s3);
+  char s1[100];
+  char s2[100] = "hello";
+  char s3[100] = "goodbye";
+  
+  printf("start strings:\n");
+  printf("s1: [%s]\n", s1);
+  printf("s2: [%s]\n", s2);
+  printf("s3: [%s]\n", s3);
+  
+  printf("\nTesting strlen(s2):\n");
+  printf("[standard]:\t%lu\n", strlen(s2) );
+  printf("[mine]:\t\t%d\n", mystrlen(s2) );
 
-    printf("\n");
-    printf("Testing strlen(s2):\n");
-    printf("[standard]: %ld\n", strlen(s2));
-    printf("[mine]: %d\n", mystrlen(mys2));
+  printf("\nTesting strcpy(s1, s2):\n");
+  printf("[standard]:\t[%s]\n", strcpy(s1, s2) );
+  printf("[mine]:\t\t[%s]\n", mystrcpy(s1, s2) );
 
-    printf("\n");
-    printf("Testing strcpy(s1, s2):\n");
-    printf("[standard]: [%s]\n", strcpy(s1, s2));
-    printf("[mine]: [%s]\n", mystrcpy(mys1, mys2));
+  printf("\nTesting strncpy(s1, s3, 3):\n");
+  printf("[standard]:\t[%s]\n", strncpy(s1, s3, 3) );
+  printf("[mine]:\t\t[%s]\n", mystrncpy(s1, s3, 3) );
+  
+  printf("\nTesting strcat(s1, s3):\n");
+  printf("[standard]:\t[%s]\n", strcat(s1, s3) );
+  s1[5] = 0;
+  printf("[mine]:\t\t[%s]\n", mystrcat(s1, s3) );
 
-    printf("\n");
-    printf("Testing strncpy(s1, s3, 3):\n");
-    printf("[standard]: [%s]\n", strncpy(s1, s3, 3));
-    printf("[mine]: [%s]\n", mystrncpy(mys1, mys3, 3));
+  printf("\nTesting strncat(s1, s2, 3):\n");
+  printf("[standard]:\t[%s]\n", strncat(s1, s2, 3) );
+  s1[12] = 0;
+  printf("[mine]:\t\t[%s]\n", mystrncat(s1, s2, 3) );
 
-    printf("\n");
-    printf("Testing strcat(s1, s3):\n");
-    printf("[standard]: [%s]\n", strcat(s1, s3));
-    printf("[mine]: [%s]\n", mystrcat(mys1, mys3));
+  printf("\nTesting strchr(s1, 'l'):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 'l') );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 'l') );
+  
+  printf("\nTesting strchr(s1, 0):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 0) );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 0) );
 
-    printf("\n");
-    printf("Testing strncat(s1, s2, 3):\n");
-    printf("[standard]: [%s]\n", strncat(s1, s2, 3));
-    printf("[mine]: [%s]\n", mystrncat(mys1, mys2, 3));
+  printf("\nTesting strchr(s1, 'z'):\n");
+  printf("[standard]:\t[%p]\n", strchr(s1, 'z') );
+  printf("[mine]:\t\t[%p]\n", mystrchr(s1, 'z') );
 
-    printf("\n");
-    printf("Comparing ab to abc:\n");
-    printf("[standard]: [%d]\n", strcmp("ab", "abc"));
-    printf("[mine]: [%d]\n", mystrcmp("ab","abc"));
+  printf("\nTesting strcmp\n");
+  printf("\tComparting ab to abc:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("ab", "abc") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("ab", "abc") );
+  printf("\tComparting abc to ab:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("abc", "ab") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("abc", "ab") );
+  printf("\tComparting abc to abc:\n");
+  printf("\t\t[standard]:\t[%d]\n", strcmp("abc", "abc") );
+  printf("\t\t[mine]:\t\t[%d]\n", mystrcmp("abc", "abc") );
 
-    printf("\n");
-    printf("Comparing abc to ab:\n");
-    printf("[standard]: [%d]\n", strcmp("abc", "ab"));
-    printf("[mine]: [%d]\n", mystrcmp("abc","ab"));
-
-    printf("\n");
-    printf("Comparing abc to abc:\n");
-    printf("[standard]: [%d]\n", strcmp("abc", "abc"));
-    printf("[mine]: [%d]\n", mystrcmp("abc","abc"));
-    
-    printf("\n");
-    printf("Testing strchr(s1, \'l\'):\n");
-    printf("[standard]: [%x]\n", strchr(s1, 'l'));
-    printf("[mine]: [%x]\n", mystrchr(s1, 'l'));
-    
-    printf("\n");
-    printf("Testing strchr(s2, \'h\'):\n");
-    printf("[standard]: [%x]\n", strchr(s2, 'h'));
-    printf("[mine]: [%x]\n", mystrchr(s2, 'h'));
-
-    printf("\n");
-    printf("Testing strchr(s3, \'o\'):\n");
-    printf("[standard]: [%x]\n", strchr(s3, 'o'));
-    printf("[mine]: [%x]\n", mystrchr(s3, 'o'));
-
-    return 0;
+  return 0;
 }
+  

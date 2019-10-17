74849df5925ea64d010374fdf15647fa0402f2eb
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-17 11:20:50 -0400

    deleted directories

--- make-kazijamal/header.h
+++
@@ -1,3 +0,0 @@
-float avg(int *arr, int arr_len);
-void arrcp(int *arr1, int *arr2, int arr_len);
-int lenstr(char *str);
--- strings-kazijamal/header.h
+++
@@ -1,7 +0,0 @@
-int mystrlen(char *s);
-char *mystrcpy(char *dest, char *src);
-char *mystrncpy(char *dest, char *src, size_t n);
-char *mystrcat(char *dest, char *src);
-char *mystrncat(char *dest, char *src, size_t n);
-int mystrcmp(char *s1, char *s2);
-char *mystrchr(char s[], char c);

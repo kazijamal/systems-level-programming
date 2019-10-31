7841059011fc756f373ff9376b39cec09d1a7e05
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-31 11:33:40 -0400

    updated song_node header

--- songnode.h
+++ songnode.h
@@ -8,4 +8,7 @@ struct song_node * insert_front(struct node * list, char * name, char * artist);
 struct song_node * insert_ordered(struct node * list, char * name, char * artist);
 void print_list(struct node * list);
 struct song_node * find_song(struct node * list, char * name, char * artist);
-struct song_node * 
+struct song_node * find_arist_first(struct node * list, char * artist);
+struct song_node * rand_node(struct node * list);
+void * remove_node(struct node * list, char * name, char * artist);
+struct song_node * free_list(struct node * list);

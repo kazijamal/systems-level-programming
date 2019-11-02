e4b741ef351718a01e641589e29a689f91057dbc
Michael Robertson <mrobertson00@stuy.edu>
2019-11-02 12:58:17 -0400

    i forgot semilcolns

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main() {
   struct song_node * list = NULL;
   print_list(list);
 
-  printf("testing adds to null list")
+  printf("testing adds to null list");
   list = insert_ordered(list, "no games", "dua lipa");
   print_list(list);
 
-  printf("testing adds to front if 1st node has greater artist")
+  printf("testing adds to front if 1st node has greater artist");
   list = insert_ordered(list, "pump it", "black eyed peas");
   print_list(list);
 
-  printf("testing adds to front if 1st node has same artist, greater song")
+  printf("testing adds to front if 1st node has same artist, greater song");
   list = insert_ordered(list, "another song", "black eyed peas");
   print_list(list);
 

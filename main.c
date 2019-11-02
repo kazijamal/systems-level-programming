f91bba2ca61343f92c8ad364fa92dfc43fa17c9c
Michael Robertson <mrobertson00@stuy.edu>
2019-11-02 12:57:41 -0400

    plz wurk

--- main.c
+++ main.c
@@ -5,9 +5,18 @@
 int main() {
   struct song_node * list = NULL;
   print_list(list);
+
+  printf("testing adds to null list")
   list = insert_ordered(list, "no games", "dua lipa");
   print_list(list);
-  list = insert_ordered(list, "flex", "mumble rapper");
+
+  printf("testing adds to front if 1st node has greater artist")
+  list = insert_ordered(list, "pump it", "black eyed peas");
   print_list(list);
+
+  printf("testing adds to front if 1st node has same artist, greater song")
+  list = insert_ordered(list, "another song", "black eyed peas");
+  print_list(list);
+
   return 0;
 }

24cef2ae312f8d35e46f782d10be8d3c3e6996cf
Michael Robertson <mrobertson00@stuy.edu>
2019-11-02 13:57:43 -0400

    headers:

--- songnode.h
+++ songnode.h
@@ -12,3 +12,5 @@ struct song_node * find_arist_first(struct song_node * list, char * artist);
 struct song_node * rand_node(struct song_node * list);
 void * remove_node(struct song_node * list, char * name, char * artist);
 struct song_node * free_list(struct song_node * list);
+struct song_node * find(struct song_node * list, char * name, char * artist);
+struct song_node * find(struct song_node * list, char * artist);

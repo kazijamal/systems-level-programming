14c3d4a6c78347d53a0a83b88ff172ec3d084484
Michael Robertson <mrobertson00@stuy.edu>
2019-11-02 12:48:45 -0400

    mega bruh

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main() {
   struct song_node * list = NULL;
   print_list(list);
-  list = insert_front(list, "no games", "dua lipa");
+  list = insert_ordered(list, "no games", "dua lipa");
   print_list(list);
   list = insert_ordered(list, "flex", "mumble rapper");
   print_list(list);
